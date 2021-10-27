957e25eeaf149fc7f231cd13eff459a3a95ad012
GooseterV <goosevincent@gmail.com>
2021-10-26 20:00:02 -0400

    g

--- insert.js
+++ insert.js
@@ -1,26 +1,20 @@
-const head_append = `
+const headAppend = `
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
-
 body>.skiptranslate {
     display: none;
 }
-
 .goog-te-banner-frame.skiptranslate {
     display: none !important;
 }
-
-
 @media print {
     #google_translate_element {
         display: none;
     }
 }
-
 .goog-te-gadget a {
     display:none;
 }
-
 #google_translate_element select {
     background-color: #409ff7;
     color: #000000;
@@ -30,7 +24,6 @@ body>.skiptranslate {
     font-family: 'Nunito';
     font-weight: 700;
 }
-
 #google_translate_element select option {
     color: #000000;
     font-family: 'Nunito';
@@ -39,7 +32,6 @@ body>.skiptranslate {
 .goog-te-gadget > * {
     left:-50%;
 }
-
 #google_translate_element select {
     outline: none;
     border: none;
@@ -47,33 +39,37 @@ body>.skiptranslate {
 #google_translate_element select:hover {
     cursor:pointer !important;
 }
-
 body {
     top: 0px !important;
 }
-
-
-
 </style>
 `;
-function formatPage(){
-    document.firstElementChild.style.removeProperty("height")
-    document.body.style.removeProperty("position")
-    document.body.style.removeProperty("top")
-}
-setTimeout(formatPage, 250)
-document.firstElementChild.translate="yes"
-document.head.innerHTML += head_append
-var translator_element = document.createElement("div");
-translator_element.id = "google_translate_element"
-translator_element.style.zIndex = 1
-translator_element.style.position = "absolute"
-translator_element.style.left = "25%"
-document.body.insertBefore(translator_element, document.body.firstElementChild)
-var script = document.createElement("script");
+
+function formatPage() {
+    document.firstElementChild.style.removeProperty("height");
+    document.body.style.removeProperty("position");
+    document.body.style.removeProperty("top");
+};
+
+setTimeout(formatPage, 250);
+
+document.firstElementChild.translate = "yes"
+document.head.innerHTML += headAppend
+
+const translatorElement = document.createElement("div");
+translatorElement.id = "google_translate_element"
+translatorElement.style.zIndex = 1
+translatorElement.style.position = "absolute"
+translatorElement.style.left = "25%"
+
+document.body.insertBefore(translatorElement, document.body.firstElementChild);
+
+const script = document.createElement("script");
 script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+
 document.getElementsByTagName("head")[0].appendChild(script);
-var script2 = document.createElement("script");
+
+const script2 = document.createElement("script");
 script2.innerText = `
         function googleTranslateElementInit() {
             new google.translate.TranslateElement({
@@ -82,8 +78,10 @@ script2.innerText = `
         }
 `
 document.getElementsByTagName("head")[0].appendChild(script2);
-function translatorRemoveChildNodes(){
-    translator_element.childNodes[0].childNodes[1].remove()
-}
 
-setTimeout(translatorRemoveChildNodes, 250)
+
+function translatorRemoveChildNodes() {
+    translatorElement.childNodes[0].childNodes[1].remove();
+};
+
+setTimeout(translatorRemoveChildNodes, 250);
